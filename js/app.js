b7d177928dc31f5a14bc88d625cec645f8d86f54
EWalles <ejomcdonald@gmail.com>
2022-08-24 14:55:57 -0400

    fix

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// add event listener new-skill
+let skill;
+
+const getSkill = (evt)=> {
+    skill = evt.target.value
+    evt.target.value  = ''
+}
+
+const submitSkill = (evt) => {
+    $('ul').append(
+        `<li>
+        <button class="delete" type="button">X</button><span>${skill}</span>          
+        </li>`) 
+}
+
+const deleteSkill = (evt) => {
+    $(evt.target).parent().html('')
+    console.log(this)
+    console.log($(evt.target).parent() )
+}
+
+$('#add-skill').on('click', submitSkill)
+$('#skill').on('blur', getSkill)
+$('ul').on('click', deleteSkill)
+//capture input in text box
+
+//on click add skill to UL
+
+
